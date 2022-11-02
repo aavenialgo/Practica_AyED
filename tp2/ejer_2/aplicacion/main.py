86864685d683f7e0253c5cd034a7b99946a7f9b3
Andres Antonio Venialgo <63910897+aavenialgo@users.noreply.github.com>
2022-11-02 01:35:00 -0300

    Agrego un main
    Agrego un script para probar la clase

--- tp2/ejer_2/aplicacion/main.py
+++ tp2/ejer_2/aplicacion/main.py
@@ -6,7 +6,38 @@ Created on Wed Nov  2 00:17:32 2022
 """
 from modulos.Temperaturas_DB import TemperaturasDB 
 
-medidas = TemperaturasDB()
+a = TemperaturasDB()
+
+
+a.guardar_temperatura("14/07/2000",20)
+a.guardar_temperatura("1/02/2000",45)
+a.guardar_temperatura("24/04/2000",6)
+
+a.guardar_temperatura("17/02/2028",43)
+a.guardar_temperatura("22/04/2020",12)
+a.guardar_temperatura("12/05/2004",25)
+#--------------------------------------------------------------------
+print("\n------Cantidad de muestras-----------")
+a.mostrar_cantidad_muestras()
+#--------------------------------------------------------------------
+print("---Mostrar Temperaturas entre dos fechas---")
+a.mostrar_temperaturas("14/07/2000", "17/02/2028")
+#--------------------------------------------------------------------
+print("\n---Temperatura maxima entre dos rangos de fecha---")
+max_temp = a.max_temp_rango("1/02/2000", "17/02/2028")
+print("Maxima temperatura",max_temp)
+#--------------------------------------------------------------------
+print("\n---Temperatura minima entre dos rangos de fecha---")
+min_temp = a.min_temp_rango("1/02/2000", "17/02/2028")
+print("Minima temperatura",min_temp)
+#--------------------------------------------------------------------
+print("\n------temp_extremos_rango-------")
+maximo,minimo= a.temp_extremos_rango("1/02/2000", "17/02/2028")
+print(maximo,minimo)
+#--------------------------------------------------------------------
+print("\n----Eliminar Temperatura en una fecha-----")
+a.borrar_temperatura("14/07/2000")
+a.mostrar_temperaturas("1/02/2000", "17/02/2028")
 
 
 
--- tp2/ejer_2/modulos/Temperaturas_DB.py
+++ tp2/ejer_2/modulos/Temperaturas_DB.py
@@ -104,52 +104,9 @@ class TemperaturasDB():
         print(self.arbol.tamano)
             
             
-if __name__ == "__main__":
-    a = TemperaturasDB()
+
     
     
 
-#--------------------------------------------------
-
- 
-    a.guardar_temperatura("14/07/2000",20)
-    a.guardar_temperatura("1/02/2000",45)
-    a.guardar_temperatura("24/04/2000",6)
-    
-    a.guardar_temperatura("17/02/2028",43)
-    a.guardar_temperatura("22/04/2020",12)
-    a.guardar_temperatura("12/05/2004",25)
-    #--------------------------------------------------------------------
-    print("\n------Cantidad de muestras-----------")
-    a.mostrar_cantidad_muestras()
-    #--------------------------------------------------------------------
-    print("---Mostrar Temperaturas entre dos fechas---")
-    a.mostrar_temperaturas("14/07/2000", "17/02/2028")
-    #--------------------------------------------------------------------
-    print("\n---Temperatura maxima entre dos rangos de fecha---")
-    max_temp = a.max_temp_rango("1/02/2000", "17/02/2028")
-    print("Maxima temperatura",max_temp)
-    #--------------------------------------------------------------------
-    print("\n---Temperatura minima entre dos rangos de fecha---")
-    min_temp = a.min_temp_rango("1/02/2000", "17/02/2028")
-    print("Minima temperatura",min_temp)
-    #--------------------------------------------------------------------
-    print("\n------temp_extremos_rango-------")
-    maximo,minimo= a.temp_extremos_rango("1/02/2000", "17/02/2028")
-    print(maximo,minimo)
-    #--------------------------------------------------------------------
-    print("\n----Eliminar Temperatura en una fecha-----")
-    a.borrar_temperatura("14/07/2000")
-    a.mostrar_temperaturas("1/02/2000", "17/02/2028")
-    
-    
-    
-       
-    # 2017-04-20 25
-    # 2017-05-04 32
-    # 2017-02-22 40
-    # 2017-03-28 23
-    # 2017-05-07 12
-    # 2017-04-29 27
     
     
